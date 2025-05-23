5573d3fda3f7604c597782bcf9fde96c74f8eb09
JRON24 <98704058+JRON24@users.noreply.github.com>
2025-05-23 08:12:07 +0800

    Commit Alpha

---
+++ app/picture.ts
@@ -0,0 +1,38 @@
+export type Picture = {
+  id: number;
+  uri: string; 
+  answer: string;
+};
+
+export const pictures: Picture[] = [
+    { id: 1, uri: require('@/assets/images/apple.png'), answer: 'apple' },
+    { id: 2, uri: require('@/assets/images/dog.png'), answer: 'dog' },
+    { id: 3, uri: require('@/assets/images/banana.png'), answer: 'banana' },
+    { id: 4, uri: require('@/assets/images/cat.png'), answer: 'cat' },
+    { id: 5, uri: require('@/assets/images/car.png'), answer: 'car' },
+    { id: 6, uri: require('@/assets/images/moon.png'), answer: 'moon' },
+    { id: 7, uri: require('@/assets/images/house.png'), answer: 'house' },
+    { id: 8, uri: require('@/assets/images/ball.png'), answer: 'ball' },
+    { id: 9, uri: require('@/assets/images/flower.png'), answer: 'flower' },
+    { id: 10, uri: require('@/assets/images/fan.png'), answer: 'fan' },
+    { id: 11, uri: require('@/assets/images/clock.png'), answer: 'clock' },
+    { id: 12, uri: require('@/assets/images/phone.png'), answer: 'phone' },
+    { id: 13, uri: require('@/assets/images/book.png'), answer: 'book' },
+    { id: 14, uri: require('@/assets/images/sun.png'), answer: 'sun' },
+    { id: 15, uri: require('@/assets/images/ice.png'), answer: 'ice' }, //
+    { id: 16, uri: require('@/assets/images/elephant.png'), answer: 'elephant' },
+    { id: 17, uri: require('@/assets/images/umbrella.png'), answer: 'umbrella' },
+    { id: 18, uri: require('@/assets/images/rocket.png'), answer: 'rocket' },
+    { id: 19, uri: require('@/assets/images/balloon.png'), answer: 'balloon' },
+    { id: 20, uri: require('@/assets/images/keyboard.png'), answer: 'keyboard' },
+    { id: 21, uri: require('@/assets/images/television.png'), answer: 'television' },
+    { id: 22, uri: require('@/assets/images/crocodile.png'), answer: 'crocodile' },
+    { id: 23, uri: require('@/assets/images/penguin.png'), answer: 'penguin' },
+    { id: 24, uri: require('@/assets/images/rocket.png'), answer: 'rocket' },
+    { id: 25, uri: require('@/assets/images/human.png'), answer: 'human' },
+    { id: 26, uri: require('@/assets/images/library.png'), answer: 'library' },
+    { id: 27, uri: require('@/assets/images/universe.png'), answer: 'universe' },
+    { id: 28, uri: require('@/assets/images/planets.png'), answer: 'planets' },
+    { id: 29, uri: require('@/assets/images/xylophone.png'), answer: 'xylophone' },
+    { id: 30, uri: require('@/assets/images/life.png'), answer: 'life' },
+];
